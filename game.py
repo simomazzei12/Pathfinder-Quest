466acaf740ce52a4719a8362ac2ab08647c1b0fa
simomazzei12 <simone.mazzei@student.unisg.ch>
2024-12-05 19:40:36 +0100

    game-v2.py

--- game.py
+++ game.py
@@ -44,8 +44,8 @@ def display_intro_screen():
     rules = [
         "INSTRUCTIONS:",
         "1. The 'value' of each path is shown as a number on the given line",
-        "2. The goal is to find path with the shortest total 'value'",
-        "3. Click on nodes to create a path",
+        "2. The goal is to find the path with the shortest total 'value'",
+        "3. Click on the nodes to create a path",
         "4. Press 'Undo' to remove the last selected node",
         "5. Press 'Show Solution' to see the correct path",
         "6. Close the window to end the game",
